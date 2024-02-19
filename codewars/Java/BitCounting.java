312651c8fcdac7ecf837c425f45da90ddfd6d8f8
Hyeonqz <wlsgusrb78@naver.com>
2024-02-19 15:41:28 +0900

    fix : codeWars 6kyu

--- codewars/Java/BitCounting.java
+++ codewars/Java/BitCounting.java
@@ -1,16 +1,4 @@
 public class BitCounting {
-    public static void main(String[] args) {
-        System.out.println(countBits(1234));
-        System.out.println(countBits(4));
-        System.out.println(countBits(7));
-        System.out.println(countBits(9));
-        System.out.println(countBits(10));
-    }
-
-    // 정수를 입력받고, 해당 숫자의
-    // 주어진 숫자를, 이진수로 변환한다.
-    // 이진수에서 1의 갯수를 구한다.
-
     public static int countBits(int n) {
         int count = 0;
         String value = Integer.toBinaryString(n);
