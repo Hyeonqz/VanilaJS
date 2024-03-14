63f7b0400a187e3153868c7678292d6168f864f9
Hyeonqz <wlsgusrb78@naver.com>
2024-03-14 19:28:36 +0900

    [Todo] : grammar basic

--- FirstJS/app/app.js
+++ FirstJS/app/app.js
@@ -5,3 +5,46 @@ console.log(5*2);
 console.log(5/2);
 
 const a = 5; // const - > 불변의 수
+
+console.log()
+
+const playerName = "nico";
+const playerPoints = 121212;
+const playerHandsome = false;
+const playerFat = "little bit";
+
+const player = ["nico",1212,false,"little bit"];
+console.log(player)
+
+const player1 = {
+  name : "nico",
+  points : 1212,
+  handsome : false,
+  fat : "little bit"
+}
+console.log(player1)
+console.log(player1["name"])
+
+function sayHello() {
+  console.log("hello hi");
+}
+
+sayHello();
+
+function sayHello2(nameOfPerson,age) {
+  console.log(nameOfPerson,age);
+}
+
+sayHello2("jin",25);
+sayHello2("hyun",30);
+sayHello2("kyu",50);
+
+function plus(a,b) {
+  console.log(a,b);
+}
+plus();
+
+function divide(a,b) {
+  console.log(a/b);
+}
+divide(10/5);
---
+++ FirstJS/app/js/Ex1.js
@@ -0,0 +1,9 @@
+const player = {
+  name : "kyu",
+  sayHello : function (otherPersonName) {
+    console.log("hello " + otherPersonName + " nice to meet you");
+  },
+};
+
+player.sayHello("jin");
+player.sayHello("hyun");
