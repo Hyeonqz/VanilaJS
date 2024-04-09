b502b0a3ba0d4f179b7bbbaf8171d298ba6323b7
Hyeonqz <wlsgusrb78@naver.com>
2024-04-09 12:39:51 +0900

    event

---
+++ FirstJS/app/js/browser/documentEx.js
@@ -0,0 +1,48 @@
+
+// 클래스 이름 로 찾기
+const hellos = document.getElementsByClassName("hello");
+console.log(hellos);
+
+// 태그 이름으로 찾기
+const title = document.getElementsByTagName("h1");
+console.log(title);
+
+// 쿼리 셀렉터 -> 중복 일시 맨처음 것만 가져옴
+const title2 = document.querySelector(".hello h1");
+const title0 = document.querySelector("#hello");
+
+title2.innerText = "I m so hungry";
+
+console.log(title0);
+console.log(title2);
+
+// 쿼리 셀렉터 전체 가져오기
+const title3 = document.querySelectorAll(".hello h1");
+console.log(title3);
+
+const title4 = document.querySelector(".hello:first-child h1");
+console.log(title4);
+
+title4.style.color = "white";
+
+
+function handleTitleClick() {
+    console.log("title4 was clicked!");
+}
+title4.addEventListener("click",handleTitleClick); // 함수 괄호는 넣으면 안됌.
+
+console.dir(title4);
+
+function handleMouserEnter() {
+    title4.innerText = "Mouse is here";
+}
+
+title4.addEventListener("mouseenter", handleMouserEnter);
+
+function handlerMouseLeave() {
+    title4.innerHTML = "Moues is gone";
+}
+title4.addEventListener("mouseleave", handlerMouseLeave);
+
+
+// TODO : #3.5 부터 보기
