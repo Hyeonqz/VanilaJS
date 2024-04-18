7571abac322c5dbef23cf48466090c444c8e9c7f
Hyeonqz <wlsgusrb78@naver.com>
2024-04-18 12:36:34 +0900

    #4.4

--- FirstJS/youtube/login.js
+++ FirstJS/youtube/login.js
@@ -1,12 +1,78 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
 
-function handleLoginButtonClick() {
+/*function handleLoginButtonClick() {
     console.dir(loginInput);
     console.log("click");
+}*/
+
+/*function onLoginBtnClick() {
+    const username = loginInput.value;
+    console.log(username);
+}*/
+
+/*const link = document.querySelector("a");
+
+function handleLinkClick(event) {
+    event.preventDefault(); // 다음 이벤트가 실행되지 못하게 막는다.
+    console.dir();
+}
+link.addEventListener("click",handleLinkClick);*/
+
+const HIDDEN_CLASSNAME = "hidden";
+const greeting = document.querySelector("#greeting");
+
+function onLoginSubmit(info) {
+    info.preventDefault(); // 어떠한 이벤트도 발생하지 않도록 막는 것.
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText=`Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
-loginButton.addEventListener("click",handleLoginButtonClick)
+loginForm.addEventListener("submit",onLoginSubmit)
 
-// TODO 4.1 부터 보기
+
+// TODO #4.5 부터 보기
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
